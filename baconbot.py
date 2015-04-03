a8c64cc9e21879d1ed5534ca9329210874bafb55
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-04-03 15:11:20 +0200

    Dropping raw codes

--- baconbot.py
+++ baconbot.py
@@ -5,16 +5,6 @@ import asyncio
 import irc3
 from irc3.plugins.command import command
 
-codes = {
-    'A_ON': "282 846 282 846 282 846 846 282 282 846 282 846 282 846 846 282 282 846 282 846 282 846 282 846 282 846 846 282 282 846 846 282 282 846 846 282 282 846 846 282 282 846 282 846 282 846 846 282 282 9588",
-    'A_OFF': "280 840 280 840 280 1120 840 280 280 1120 280 840 280 840 840 280 280 840 280 840 280 1120 280 840 280 840 840 280 280 840 840 280 280 840 840 280 280 840 840 280 280 840 840 280 280 840 280 840 280 9520",
-    'B_ON': "282 846 282 846 282 846 846 282 282 846 282 846 282 846 846 282 282 1128 282 846 282 846 846 282 282 846 282 846 282 846 846 282 282 846 846 282 282 846 846 282 282 846 282 1128 282 846 846 282 282 9588",
-    'B_OFF': "281 843 281 843 281 843 843 281 281 843 281 843 281 843 843 281 281 843 281 1124 281 843 843 281 281 843 281 843 281 843 843 281 281 843 843 281 281 843 843 281 281 843 843 281 281 1124 281 843 281 9554",
-    'C_ON': "282 846 282 846 282 846 846 282 282 846 282 846 282 846 846 282 282 1128 282 846 282 846 1128 282 282 1128 846 282 282 846 282 846 282 846 846 282 282 846 846 282 282 846 282 1128 282 846 846 282 282 9588",
-    'C_OFF': "282 846 282 846 282 846 846 282 282 846 282 846 282 1128 846 282 282 846 282 1128 282 846 846 282 282 846 846 282 282 846 282 846 282 846 846 282 282 846 846 282 282 846 846 282 282 846 282 846 282 9588",
-    'D_ON': "282 846 282 846 282 846 846 282 282 846 282 846 282 846 846 282 282 846 282 846 282 846 846 282 282 846 846 282 282 846 846 282 282 846 282 846 282 846 846 282 282 846 282 846 282 846 846 282 282 9588",
-    'D_OFF': "281 843 281 843 281 843 843 281 281 843 281 843 281 843 843 281 281 843 281 843 281 843 843 281 281 843 843 281 281 843 843 281 281 843 281 843 281 843 843 281 281 843 843 281 281 843 281 843 281 9554",
-}
 
 logging.basicConfig(filename='bot.log',level=logging.DEBUG)
 
@@ -26,61 +16,74 @@ class AlarmPlugin:
     @command
     def love(self, mask, target, args):
         """Love command
-        %%love [TIME]
+        %%love [ARG]
         """
-        time = min(float(args['TIME'] or 5), 3600)
-        logging.info('Starting love for {} seconds'.format(time))
-        self.bot.privmsg(target, 'Så er der kærlighed i {} sekunder!'.format(time))
-        self.bot.loop.create_task(self.process_love(target, time))
-
-    @asyncio.coroutine
-    def process_love(self, target, time):
-        self.bot.loop.create_task(self.send_signal(target, 'C_ON'))
-        yield from asyncio.sleep(time)
-        for _ in range(50):
-            self.bot.loop.create_task(self.send_signal(target, 'C_OFF'))
+        arg = args.get('ARG', None)
+        if arg:
+            logging.info('Starting love for {} seconds'.format(arg))
+            # self.bot.notice(mask.nick, 'Så er der kærlighed i {} sekunder!'.format(arg))
+        self.bot.loop.create_task(
+            self.process_maybe_timed_command(
+                target, 4, arg
+            )
+        )
 
     @command
     def alarm(self, mask, target, args):
         """Alarm command
-        %%alarm [TIME]
+        %%alarm [ARG]
         """
-        time = min(float(args['TIME'] or 5), 3600)
-        logging.info('Starting alarm for {} seconds'.format(time))
-        self.bot.privmsg(target, 'ALARM ALARM ALARM I {} SEKUNDER!'.format(time))
-        self.bot.loop.create_task(self.process_alarm(target, time))
-
-    @asyncio.coroutine
-    def process_alarm(self, target, time):
-        for _ in range(50):
-            self.bot.loop.create_task(self.send_signal(target, 'A_ON'))
-        yield from asyncio.sleep(time, loop=self.bot.loop)
-        for _ in range(50):
-            self.bot.loop.create_task(self.send_signal(target, 'A_OFF'))
+        arg = args.get('ARG', None)
+        if arg:
+            logging.info('Starting alarm for {} seconds'.format(arg))
+            # self.bot.notice(mask.nick, 'ALARM ALARM ALARM I {} SEKUNDER!'.format(arg))
+        self.bot.loop.create_task(
+            self.process_maybe_timed_command(
+                target, 1, arg
+            )
+        )
 
     @command
     def illuminate(self, mask, target, args):
         """Illuminate command
         %%illuminate
         """
-        self.bot.privmsg(target, 'Illuminate!')
+        self.bot.notice(mask.nick, 'Illuminate!')
         logging.info('Illuminate')
-        for _ in range(50):
-            self.bot.loop.create_task(self.send_signal(target, 'D_ON'))
+        self.bot.loop.create_task(self.send_signal(target, 8, 't'))
 
     @command
     def deluminate(self, mask, target, args):
         """Deluminate command
         %%deluminate
         """
-        self.bot.privmsg(target, 'Deluminate!')
+        self.bot.notice(mask.nick, 'Deluminate!')
         logging.info('Deluminate')
-        for _ in range(50):
-            self.bot.loop.create_task(self.send_signal(target, 'D_OFF'))
+        self.bot.loop.create_task(self.send_signal(target, 8, 'f'))
 
     @asyncio.coroutine
-    def send_signal(self, target, code_id):
-        base_command = 'sudo pilight-send -p raw -c "{}"'
-        command = base_command.format(codes[code_id])
+    def process_maybe_timed_command(self, target, unit, arg=None):
+        OFF_WORDS = ['off', 'false', 'f']
+        timed = False
+
+        if not arg or arg not in OFF_WORDS:
+            self.bot.loop.create_task(self.send_signal(target, unit, 't'))
+
+        try:
+            if int(arg) > 0:
+                yield from asyncio.sleep(min(int(arg), 3600), loop=self.bot.loop)
+                timed = True
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+
+        if arg in OFF_WORDS or timed:
+            self.bot.loop.create_task(self.send_signal(target, unit, 'f'))
+
+    @asyncio.coroutine
+    def send_signal(self, target, code_id, state):
+        base_command = 'sudo pilight-send -p elro_800_switch -s 21 -u {} -{}'
+        command = base_command.format(code_id, state)
         logging.debug('{}: {}: {}'.format(datetime.now(), code_id, command))
         yield from asyncio.create_subprocess_shell(command, loop=self.bot.loop)
